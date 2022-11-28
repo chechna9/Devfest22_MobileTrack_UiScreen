bb5c2d557a03380ff9ce21dd2a5e5c3dd2057ae5
chechna9 <ja_bouchouareb@esi.dz>
2022-11-28 21:48:04 +0100

    upper in events done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:devfest_challenges/pages/events.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const Material(child: Events()),
     );
   }
 }
---
+++ lib/pages/events.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Events extends StatelessWidget {
+  const Events({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background.png'),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.contain,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [Upper(), Container()],
+      ),
+    );
+  }
+}
+
+class Upper extends StatelessWidget {
+  const Upper({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          left: -30,
+          bottom: 0,
+          child: Image.asset(
+            'assets/Lampe.png',
+            width: 60,
+          ),
+        ),
+        Positioned(
+          right: -70,
+          top: 0,
+          child: Image.asset('assets/Maqam_el_chahid.png'),
+        ),
+        RichText(
+          text: const TextSpan(
+            text: 'CURRENT ',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+            children: <TextSpan>[
+              TextSpan(text: 'E', style: TextStyle(color: Colors.blue)),
+              TextSpan(text: 'VENTS'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
