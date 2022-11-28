e429a054378b582cd249f496fbc73c05c92e1ab2
chechna9 <ja_bouchouareb@esi.dz>
2022-11-28 21:01:05 +0100

    init repo

---
+++ android/app/src/main/kotlin/com/example/devfest_challenges/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.devfest_challenges
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text(
+          "Azul World",
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
