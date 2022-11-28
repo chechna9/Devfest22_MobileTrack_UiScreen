a4bf91d8335b2514b56da1f3e468da5c3659bc4e
chechna9 <ja_bouchouareb@esi.dz>
2022-11-28 23:12:30 +0100

    event card done

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -6,7 +6,7 @@ class Events extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       decoration: const BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/background.png'),
@@ -17,7 +17,97 @@ class Events extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Upper(), Container()],
+        children: [
+          // upper side
+          const Expanded(
+            child: Upper(),
+          ),
+          // event info
+          Expanded(
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/background5.png'),
+                    alignment: Alignment.topCenter,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/background4.png'),
+                      alignment: Alignment.topCenter,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    alignment: Alignment.center,
+                    children: [
+                      // glob image
+                      Positioned(
+                        top: 40,
+                        left: -20,
+                        child: Image.asset('assets/Globe.png'),
+                      ),
+                      // stats and other 2 other images
+                      Positioned(
+                        left: 0,
+                        bottom: 20,
+                        child: Column(
+                          children: [
+                            Image.asset('assets/Developers.png'),
+                            const Text(
+                              "+80",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 20,
+                        child: Image.asset('assets/Pointer.png'),
+                      ),
+                      // Description
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Devfest 2022",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Devfest is an anually event hel by gdg community",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
