8a43e78e4cf8d25572e3d71d2fed1e6f9646f7f7
chechna9 <ja_bouchouareb@esi.dz>
2022-11-28 23:28:54 +0100

    extract currentEventCard and make it dynamic

--- lib/components/currentEventCard.dart
+++ lib/components/currentEventCard.dart
@@ -1,1 +1,97 @@
+import 'package:flutter/material.dart';
 
+class CurrentEventCard extends StatelessWidget {
+  final String title;
+  final String desc;
+  final int participants;
+  const CurrentEventCard({
+    Key? key,
+    required this.title,
+    required this.desc,
+    required this.participants,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/background5.png'),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background4.png'),
+            alignment: Alignment.topCenter,
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.center,
+          children: [
+            // glob image
+            Positioned(
+              top: 40,
+              left: -20,
+              child: Image.asset('assets/Globe.png'),
+            ),
+            // stats and other 2 other images
+            Positioned(
+              left: 0,
+              bottom: 20,
+              child: Column(
+                children: [
+                  Image.asset('assets/Developers.png'),
+                  Text(
+                    "+$participants",
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: 20,
+              child: Image.asset('assets/Pointer.png'),
+            ),
+            // Title & Description
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  desc,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -1,3 +1,4 @@
+import 'package:devfest_challenges/components/currentEventCard.dart';
 import 'package:flutter/material.dart';
 
 class Events extends StatelessWidget {
@@ -19,92 +20,13 @@ class Events extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // upper side
-          const Expanded(
-            child: Upper(),
-          ),
+          Upper(),
           // event info
-          Expanded(
-            child: Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/background5.png'),
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/background4.png'),
-                      alignment: Alignment.topCenter,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    alignment: Alignment.center,
-                    children: [
-                      // glob image
-                      Positioned(
-                        top: 40,
-                        left: -20,
-                        child: Image.asset('assets/Globe.png'),
-                      ),
-                      // stats and other 2 other images
-                      Positioned(
-                        left: 0,
-                        bottom: 20,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/Developers.png'),
-                            const Text(
-                              "+80",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Positioned(
-                        right: 0,
-                        bottom: 20,
-                        child: Image.asset('assets/Pointer.png'),
-                      ),
-                      // Description
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            "Devfest 2022",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Devfest is an anually event hel by gdg community",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          Flexible(
+            child: CurrentEventCard(
+              title: 'Devfest 2022',
+              desc: 'Devfest is an anually event hel by gdg community',
+              participants: 80,
             ),
           ),
         ],
