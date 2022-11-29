b9c743c38d63a5c736ebc5aa956926d6891a7d1b
chechna9 <ja_bouchouareb@esi.dz>
2022-11-29 02:34:14 +0100

    create agenda models

---
+++ lib/models/action_model.dart
@@ -0,0 +1,10 @@
+class ActionModele {
+  final String time;
+  final String date;
+  final String name;
+  ActionModele({
+    required this.date,
+    required this.name,
+    required this.time,
+  });
+}
